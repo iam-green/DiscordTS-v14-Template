152ce4531354952b45ac90956515a55549fda09b
Pastel <yundohyun050121@gmail.com>
2024-06-24 18:31:55 +0900

    edit: `guild` type, service

--- src/database/service/guild.ts
+++ src/database/service/guild.ts
@@ -1,22 +1,23 @@
 import { and, asc, between, desc, eq } from 'drizzle-orm';
 import { db } from '..';
 import {
-  Guild,
-  GuildDto,
-  GuildValidate,
-  FindOptionDto,
-  FindOptionValidate,
+  FindGuildDto,
+  FindGuildValidate,
+  CreateGuildDto,
+  CreateGuildValidate,
+  UpdateGuildDto,
+  UpdateGuildValidate,
 } from '../types';
 import { guild } from '../schema';
 
 export class GuildService {
-  static async find(data: Partial<Guild>, option?: Partial<FindOptionDto>) {
-    const { id } = data;
-    const { sort, page, count, from, to } = FindOptionValidate.parse(option);
+  static async find(data: FindGuildDto) {
+    const { id, created, sort, page, count, from, to } =
+      FindGuildValidate.parse(data);
     return await db.query.guild.findMany({
       where: and(
         id ? eq(guild.id, id) : undefined,
-        between(guild.created, from, to),
+        created ? eq(guild.created, created) : between(guild.created, from, to),
       ),
       orderBy: sort == 'asc' ? [asc(guild.created)] : [desc(guild.created)],
       offset: (page - 1) * count,
@@ -30,18 +31,18 @@ export class GuildService {
     });
   }
 
-  static async create(data: GuildDto) {
+  static async create(data: CreateGuildDto) {
     await db.insert(guild).values({
-      ...(await GuildValidate.parseAsync(data)),
+      ...(await CreateGuildValidate.parseAsync(data)),
       created: new Date(),
     });
     return this.get(data.id);
   }
 
-  static async update(id: string, data: Partial<Guild>) {
+  static async update(id: string, data: UpdateGuildDto) {
     await db
       .update(guild)
-      .set(GuildValidate.omit({ id: true }).partial().parse(data))
+      .set(UpdateGuildValidate.parse(data))
       .where(eq(guild.id, id));
     return this.get(id);
   }
