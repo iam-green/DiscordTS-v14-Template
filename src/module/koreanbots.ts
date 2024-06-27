43764825ae0090a86b34cb122d3650e6483ed4ec
Pastel <yundohyun050121@gmail.com>
2024-06-27 19:53:21 +0900

    add: `koreanbots` module try - catch

--- src/module/koreanbots.ts
+++ src/module/koreanbots.ts
@@ -4,15 +4,18 @@ import { Log } from './log';
 
 export class KoreanBots {
   static async update() {
-    if (!process.env.KOREANBOTS_TOKEN) return;
-    const bot = new Koreanbots({
-      api: { token: process.env.KOREANBOTS_TOKEN! },
-      clientID: client.application!.id,
-    });
-    Log.debug('KoreanBots information has been successfully updated.');
-    return await bot.mybot.update({
-      servers: client.guilds.cache.size,
-      shards: client.shard?.count,
-    });
+    try {
+      if (!process.env.KOREANBOTS_TOKEN) return;
+      const bot = new Koreanbots({
+        api: { token: process.env.KOREANBOTS_TOKEN! },
+        clientID: client.application!.id,
+      });
+      await bot.mybot.update({
+        servers: client.guilds.cache.size,
+        shards: client.shard?.count,
+      });
+    } catch (e) {
+      Log.error(e, __filename);
+    }
   }
 }
