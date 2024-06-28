5e869d149269dee84e67e98a5b4fef766438a2cc
Pastel <yundohyun050121@gmail.com>
2024-06-28 15:22:19 +0900

    edit: `discord/index.ts`

--- src/discord/index.ts
+++ src/discord/index.ts
@@ -9,6 +9,5 @@ export * from './event';
 export const discordInit = async () => {
   await Command.registerCommands();
   await Shard.spawn();
-  await KoreanBots.update(shard);
   setInterval(async () => KoreanBots.update(shard), 1000 * 60 * 10);
 };
