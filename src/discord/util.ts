c258d65ae4eca0972f977fc3a7d17c0afed51074
iam-green <yundohyun050121@gmail.com>
2024-08-13 20:16:27 +0900

    edit: `util` discord
    `developerId` 추가 및 코드 최적화

--- src/discord/util.ts
+++ src/discord/util.ts
@@ -1,31 +1,41 @@
 import { REST, Routes } from 'discord.js';
 
 export class DiscordUtil {
+  private static expire = 0;
   private static client_id = '';
-  private static owner_id = { data: [] as string[], expire: 0 };
+  private static admin_id = [] as string[];
+  private static developer_id = [] as string[];
+
+  private static async getValues() {
+    if (!process.env.BOT_TOKEN) throw new Error('No Bot Token');
+    const rest = new REST().setToken(process.env.BOT_TOKEN);
+    const result: any = await rest.get(Routes.currentApplication());
+    this.client_id = result.id;
+    this.admin_id = result.team
+      ? result.team.members
+          .filter((v) => v.role == 'admin')
+          .map((v) => v.user.id as string)
+      : [result.owner.id as string];
+    this.developer_id = result.team
+      ? result.team.members
+          .filter((v) => v.role == 'developer')
+          .map((v) => v.user.id as string)
+      : [];
+    this.expire = Date.now() + 1000 * 60 * 60 * 4;
+  }
 
   static async clientId() {
-    if (!this.client_id) {
-      if (!process.env.BOT_TOKEN) throw new Error('No Bot Token');
-      const rest = new REST().setToken(process.env.BOT_TOKEN);
-      const result: any = await rest.get(Routes.currentApplication());
-      this.client_id = result.id;
-    }
+    if (this.expire < Date.now()) await this.getValues();
     return this.client_id;
   }
 
-  static async ownerId() {
-    if (this.owner_id.data.length < 1 || this.owner_id.expire < Date.now()) {
-      if (!process.env.BOT_TOKEN) throw new Error('No Bot Token');
-      const rest = new REST().setToken(process.env.BOT_TOKEN);
-      const result: any = await rest.get(Routes.currentApplication());
-      this.owner_id.data = result.team
-        ? result.team.members
-            .filter((v) => v.role == 'admin')
-            .map((v) => v.user.id as string)
-        : [result.owner.id as string];
-      this.owner_id.expire = Date.now() + 1000 * 60 * 60;
-    }
-    return this.owner_id.data;
+  static async adminId() {
+    if (this.expire < Date.now()) await this.getValues();
+    return this.admin_id;
+  }
+
+  static async developerId() {
+    if (this.expire < Date.now()) await this.getValues();
+    return this.developer_id;
   }
 }
