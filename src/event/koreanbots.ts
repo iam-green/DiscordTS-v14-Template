5f18bb61518e06a4e8ce8cfd00e7ba4b8c3ca7a7
iam-green <yundohyun050121@gmail.com>
2024-08-17 20:51:00 +0900

    edit: `koreanbots` event

--- src/event/koreanbots.ts
+++ src/event/koreanbots.ts
@@ -5,7 +5,7 @@ export default new ExtendedEvent({
   event: 'ready',
   once: true,
   run: async (client) => {
-    if (!process.env.KOREANBOTS_TOKEN) return;
+    if (client.cluster.id != 0 || !process.env.KOREANBOTS_TOKEN) return;
     await KoreanBots.init();
     setInterval(
       async () => await KoreanBots.update(client.cluster),
