98866d79dac6735048098ee7aed301b7d5338df5
iam-green <yundohyun050121@gmail.com>
2024-08-18 14:24:03 +0900

    fix: `voice` event
    다른 사용자가 나갔을 때 같이 나가는 버그 수정

--- src/event/voice.ts
+++ src/event/voice.ts
@@ -5,7 +5,7 @@ import { Voice } from '../module';
 
 export default new ExtendedEvent({
   event: Events.VoiceStateUpdate,
-  run: async (_, oldState, newState) => {
+  run: async (client, oldState, newState) => {
     // Check if the bot is connected to the voice channel
     if (
       getVoiceConnection(oldState.guild.id)?.joinConfig.channelId !=
@@ -16,7 +16,9 @@ export default new ExtendedEvent({
     // Check if the bot is the only one in the voice channel or the bot has left the voice channel
     if (
       oldState.channel?.members.filter((v) => !v.user.bot).size == 0 ||
-      (!!oldState.channel?.id && !newState.channel?.id)
+      (oldState.member?.id == client.user?.id &&
+        !!oldState.channel?.id &&
+        !newState.channel?.id)
     )
       Voice.quit(oldState.guild);
   },
