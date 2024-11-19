2b81166ec7792a82bd4cf41f219205a01ee7c2f8
iam-green <yundohyun050121@gmail.com>
2024-11-19 19:19:52 +0900

    add: `bot` config

---
+++ src/config/bot.ts
@@ -0,0 +1,22 @@
+import { ClientOptions, GatewayIntentBits, Options } from 'discord.js';
+
+export const BotConfig = {
+  NAME: 'Template',
+  COMMAND_PREFIX: ['/', '?', '!', '?!'],
+  CLIENT_OPTION: {
+    intents: [
+      GatewayIntentBits.MessageContent,
+      GatewayIntentBits.Guilds,
+      GatewayIntentBits.GuildMessages,
+      GatewayIntentBits.GuildVoiceStates,
+    ],
+    sweepers: {
+      ...Options.DefaultSweeperSettings,
+      messages: { interval: 3600, lifetime: 1800 },
+      users: {
+        interval: 3600,
+        filter: () => (user) => user.bot && user.id != user.client.user.id,
+      },
+    },
+  } as ClientOptions,
+};
